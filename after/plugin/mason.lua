b3dd1ce764f2b36ed9a09385a3f913a4488d6d53
Kola Campbell <kolacampbell@microsoft.com>
2023-01-03 13:15:46 -0500

    QOL changes

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,3 +1,15 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local utils = require("telescope.utils")
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", width = 100 })
+
+vim.lsp.handlers["textDocument/signatureHelp"] =
+	vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded", width = 100 })
+
 -- Setup mason so it can manage external tooling
 require("mason").setup()
 
@@ -187,6 +199,87 @@ cmp.setup({
 		end,
 	},
 })
+
+-- Telescipe UI select
+-- Code actions doesn't have a global handler so we need to roll our sleeves a bit
+--------------------------------------------------------------------------------------------------
+vim.ui.select = function(_items, opts, on_choice)
+	local items = {}
+	local low_priority_items = {}
+
+	for _, item in ipairs(_items) do
+		local low_priority = string.find(string.lower((item[2] or {}).title or "") or "", "disable")
+			and item[2].command == "NULL_LS_CODE_ACTION"
+
+		if low_priority then
+			table.insert(low_priority_items, item)
+		else
+			table.insert(items, item)
+		end
+	end
+
+	items = vim.list_extend(items, low_priority_items)
+
+	local topts = require("telescope.themes").get_cursor({
+		initial_mode = "normal",
+	})
+	opts = opts or {}
+	local prompt = vim.F.if_nil(opts.prompt, "Select one of")
+	if prompt:sub(-1, -1) == ":" then
+		prompt = prompt:sub(1, -2)
+	end
+	opts.format_item = vim.F.if_nil(opts.format_item, function(e)
+		return tostring(e)
+	end)
+
+	local sopts = {}
+	local indexed_items, widths = vim.F.if_nil(sopts.make_indexed, function(items_)
+		local indexed_items = {}
+		for idx, item in ipairs(items_) do
+			table.insert(indexed_items, { idx = idx, text = item })
+		end
+		return indexed_items
+	end)(items)
+	local displayer = vim.F.if_nil(sopts.make_displayer, function() end)(widths)
+	local make_display = vim.F.if_nil(sopts.make_display, function(_)
+		return function(e)
+			local x, _ = opts.format_item(e.value.text)
+			return x
+		end
+	end)(displayer)
+	local make_ordinal = vim.F.if_nil(sopts.make_ordinal, function(e)
+		return opts.format_item(e.text)
+	end)
+
+	pickers
+		.new(topts, {
+			prompt_title = prompt,
+			finder = finders.new_table({
+				results = indexed_items,
+				entry_maker = function(e)
+					return {
+						value = e,
+						display = make_display,
+						ordinal = make_ordinal(e),
+					}
+				end,
+			}),
+			attach_mappings = function(prompt_bufnr)
+				actions.select_default:replace(function()
+					local selection = action_state.get_selected_entry()
+					if selection == nil then
+						utils.__warn_no_selection("ui-select")
+						return
+					end
+					actions.close(prompt_bufnr)
+					on_choice(selection.value.text, selection.value.idx)
+				end)
+				return true
+			end,
+			sorter = conf.generic_sorter(topts),
+		})
+		:find()
+end
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,153 @@
+local pickers = require("telescope.pickers")
 local entry_display = require("telescope.pickers.entry_display")
 local config = CONFIG.telescope
+local utils = require("telescope.utils")
+local strings = require("plenary.strings")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local finders = require("telescope.finders")
+local action_state = require("telescope.actions.state")
+
+local checkout = function(prompt_bufnr)
+	local selection = action_state.get_selected_entry()
+	if selection == nil then
+		utils.__warn_no_selection("actions.git_switch_branch")
+		return
+	end
+	actions.close(prompt_bufnr)
+	local pattern = "^refs/remotes/%w+/"
+	local branch = selection.value
+	if string.match(selection.refname, pattern) then
+		branch = string.gsub(selection.refname, pattern, "")
+	end
+	vim.cmd("G checkout " .. branch)
+end
+
+local delete_branch = function(prompt_bufnr)
+	local selection = action_state.get_selected_entry()
+	local confirmation = vim.fn.confirm("Delete " .. selection.name, "&Yes\n&No")
+	if confirmation == 1 then
+		vim.cmd("G branch -D " .. selection.value)
+		require("kola.telescope.custom_builtins").git_branches()
+	end
+end
+
+local M = {}
+
+local git_branches = function()
+	local format = "%(HEAD)" .. "%(refname)" .. "%(authorname)" .. "%(upstream:lstrip=2)" .. "%(committerdate:relative)"
+	local output = utils.get_os_command_output({ "git", "for-each-ref", "--perl", "--format", format })
+
+	local results = {}
+	local widths = {
+		name = 0,
+		authorname = 0,
+		upstream = 0,
+		committerdate = 0,
+	}
+	local unescape_single_quote = function(v)
+		return string.gsub(v, "\\([\\'])", "%1")
+	end
+	local parse_line = function(line)
+		local fields = vim.split(string.sub(line, 2, -2), "''", true)
+		local entry = {
+			head = fields[1],
+			refname = unescape_single_quote(fields[2]),
+			authorname = unescape_single_quote(fields[3]),
+			upstream = unescape_single_quote(fields[4]),
+			committerdate = fields[5],
+		}
+		local prefix
+		if vim.startswith(entry.refname, "refs/remotes/") then
+			prefix = "refs/remotes/"
+		elseif vim.startswith(entry.refname, "refs/heads/") then
+			prefix = "refs/heads/"
+		else
+			return
+		end
+		local index = 1
+		if entry.head ~= "*" then
+			index = #results + 1
+		end
+
+		entry.name = string.sub(entry.refname, string.len(prefix) + 1)
+		for key, value in pairs(widths) do
+			widths[key] = math.max(value, strings.strdisplaywidth(entry[key] or ""))
+		end
+		if string.len(entry.upstream) > 0 then
+			widths.upstream_indicator = 2
+		end
+		table.insert(results, index, entry)
+	end
+	for _, line in ipairs(output) do
+		parse_line(line)
+	end
+	if #results == 0 then
+		return
+	end
+
+	local displayer = entry_display.create({
+		separator = " ",
+		items = {
+			{ width = 1 },
+			{ width = 70 },
+			{ width = 20 },
+			{ width = widths.upstream_indicator },
+			{ width = widths.committerdate },
+		},
+	})
+
+	local make_display = function(entry)
+		return displayer({
+			{ entry.head },
+			{ entry.name, "TelescopeResultsIdentifier" },
+			{ entry.authorname, "TelescopePreviewUser" },
+			{ string.len(entry.upstream) > 0 and "" or "" },
+			{ entry.committerdate, "TelescopePreviewDate" },
+		})
+	end
+
+	pickers
+		.new({}, {
+			prompt_title = "Git Branches",
+			finder = finders.new_table({
+				results = results,
+				entry_maker = function(entry)
+					entry.value = entry.name
+					entry.ordinal = entry.name .. " " .. entry.authorname
+					entry.display = make_display
+					return entry
+				end,
+			}),
+			previewer = false,
+			layout_strategy = "vertical",
+			layout_config = { prompt_position = "top", width = 0.4, height = 0.4 },
+			sorter = conf.file_sorter(),
+			attach_mappings = function(_, map)
+				actions.select_default:replace(checkout)
+				map("i", "<c-t>", actions.git_track_branch)
+				map("n", "<c-t>", actions.git_track_branch)
+
+				map("i", "<c-r>", actions.git_rebase_branch)
+				map("n", "<c-r>", actions.git_rebase_branch)
+
+				map("i", "<c-a>", actions.git_create_branch)
+				map("n", "<c-a>", actions.git_create_branch)
+
+				map("i", "<c-s>", actions.git_switch_branch)
+				map("n", "<c-s>", actions.git_switch_branch)
+
+				map("i", "<c-d>", delete_branch)
+				map("n", "<c-d>", actions.git_delete_branch)
+
+				map("i", "<c-y>", actions.git_merge_branch)
+				map("n", "<c-y>", actions.git_merge_branch)
+				return true
+			end,
+		})
+		:find()
+end
+
 local entry_from_quickfix = function(opts)
 	opts = opts or {}
 
@@ -120,14 +268,17 @@ require("telescope").setup({
 
 pcall(require("telescope").load_extension, "fzf")
 
+P(IS_WINDOWS and "Windows" or "MacOS")
+
 local search_vim_config = function()
 	require("telescope.builtin").find_files({
 		prompt_title = "<VimRc >",
-		cwd = "~/.config",
+		cwd = IS_WINDOWS and "C:\\Users\\kolacampbell\\dotfiles\\.config\\" or "~/.config",
 		path_display = { "truncate" },
 	})
 end
 vim.keymap.set("n", "<leader>vrc", search_vim_config, { desc = "Search dotfiles" })
+vim.keymap.set("n", "<leader>gb", git_branches, { desc = "Show git branches" })
 vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
 vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
 vim.keymap.set("n", "<leader>/", function()
