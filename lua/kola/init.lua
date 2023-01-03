f0e13a01325c7ef9757ac96e1c4e3c7976b7f523
Kola Campbell <kolacampbell@microsoft.com>
2023-01-03 12:16:32 -0500

    cleanup

--- lua/kola/init.lua
+++ lua/kola/init.lua
@@ -1,10 +1,10 @@
 -- personal json config path
 CONFIG_PATH = "$HOME/.config/nvim/settings.json"
 
+require("kola.packer")
 require("kola.config")
 require("kola.options")
 require("kola.keymaps")
 require("kola.globals")
-require("kola.packer")
 require("kola.autocommands")
 require("kola.utils")
