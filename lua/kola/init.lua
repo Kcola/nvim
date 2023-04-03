2c2ebc493bcb68eb068e76a2b0a8c9aab023ff1a
Kola Campbell <kolawole.campbell@gmail.com>
2023-04-03 02:20:32 -0400

    map leader

--- lua/kola/init.lua
+++ lua/kola/init.lua
@@ -4,6 +4,7 @@ CONFIG_PATH = "$HOME/.config/nvim/settings.json"
 require("kola.globals")
 require("kola.config")
 require("kola.packer")
+require("kola.options")
 require("kola.keymaps")
 require("kola.autocommands")
 require("kola.utils")
--- lua/kola/options.lua
+++ lua/kola/options.lua
@@ -6,8 +6,6 @@ vim.o.termguicolors = true
 vim.cmd([[colorscheme vscode]])
 vim.cmd([[set autoread]])
 vim.o.completeopt = "menuone,noselect"
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 vim.opt.backup = false -- creates a backup file
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
