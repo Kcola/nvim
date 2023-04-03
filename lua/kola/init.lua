79ed7d6eefafc4506af96ef6377c3570dba435bf
Kola Campbell <kolawole.campbell@gmail.com>
2023-04-03 02:50:14 -0400

    renamed plugin

--- lua/kola/init.lua
+++ lua/kola/init.lua
@@ -3,7 +3,7 @@ CONFIG_PATH = "$HOME/.config/nvim/settings.json"
 
 require("kola.globals")
 require("kola.config")
-require("kola.packer")
+require("kola.lazy")
 require("kola.options")
 require("kola.keymaps")
 require("kola.autocommands")
